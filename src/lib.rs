ad94dce14b094c1c70fa1262da8739aaccda5766
Joseph Mark <sjeohp@gmail.com>
2017-06-15 17:33:53 +0800

    Minor tidyup

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,8 @@
 #![crate_type = "lib"]
 
 extern crate gl;
-
 use self::gl::types::{GLenum, GLint, GLuint, GLchar, GLfloat, GLsizeiptr};
 
-use std::io::{BufReader, Read};
-use std::fs::File;
 use std::ptr;
 use std::str;
 use std::ffi::CString;
@@ -110,14 +107,12 @@ pub fn check_gl_error(file: &'static str, line: u32)
 	}
 }
 
-// pub static mut LINE_DATA: [GLfloat; 10000] = [0.; 10000];
 
 pub const STRIDE: usize = 16;
 
 pub fn add_path_line(path: &[(f32, f32)], path_edges: usize, line_program: GLuint, line_vao: GLuint, line_vbo: GLuint)
 {
-	let mut LINE_DATA: Vec<f32> = vec![0.0; path_edges*STRIDE];
-	let mut offset: usize = 0;
+	let mut line_data: Vec<f32> = Vec::with_capacity(path_edges*STRIDE);
 
 	unsafe
 	{
@@ -125,28 +120,27 @@ pub fn add_path_line(path: &[(f32, f32)], path_edges: usize, line_program: GLuin
 		{
 			let normalx = path[i+1].1 - path[i].1;
 			let normaly = path[i].0 - path[i+1].0;
-			LINE_DATA[offset+0] = path[i].0;
-			LINE_DATA[offset+1] = path[i].1;
-			LINE_DATA[offset+2] = -normalx;
-			LINE_DATA[offset+3] = -normaly;
-			LINE_DATA[offset+4] = path[i].0;
-			LINE_DATA[offset+5] = path[i].1;
-			LINE_DATA[offset+6] = normalx;
-			LINE_DATA[offset+7] = normaly;
-			LINE_DATA[offset+8] = path[i+1].0;
-			LINE_DATA[offset+9] = path[i+1].1;
-			LINE_DATA[offset+10] = -normalx;
-			LINE_DATA[offset+11] = -normaly;
-			LINE_DATA[offset+12] = path[i+1].0;
-			LINE_DATA[offset+13] = path[i+1].1;
-			LINE_DATA[offset+14] = normalx;
-			LINE_DATA[offset+15] = normaly;
-			offset += STRIDE;
+			line_data.push(path[i].0);
+			line_data.push(path[i].1);
+			line_data.push(-normalx);
+			line_data.push(-normaly);
+			line_data.push(path[i].0);
+			line_data.push(path[i].1);
+			line_data.push(normalx);
+			line_data.push(normaly);
+			line_data.push(path[i+1].0);
+			line_data.push(path[i+1].1);
+			line_data.push(-normalx);
+			line_data.push(-normaly);
+			line_data.push(path[i+1].0);
+			line_data.push(path[i+1].1);
+			line_data.push(normalx);
+			line_data.push(normaly);
 		}
 		gl::UseProgram(line_program);
 		gl::BindVertexArray(line_vao);
 		gl::BindBuffer(gl::ARRAY_BUFFER, line_vbo);
-		gl::BufferSubData(gl::ARRAY_BUFFER, 0, (path_edges * STRIDE * mem::size_of::<GLfloat>()) as GLsizeiptr as isize, mem::transmute(&LINE_DATA[0]));
+		gl::BufferSubData(gl::ARRAY_BUFFER, 0, (path_edges*STRIDE*mem::size_of::<GLfloat>()) as GLsizeiptr as isize, mem::transmute(&line_data[0]));
 		gl::BindBuffer(gl::ARRAY_BUFFER, 0);
 		gl::BindVertexArray(0);
 		gl::UseProgram(0);
